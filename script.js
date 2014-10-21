86ed5c998a939775b6c1ba760f693aaae7ca1d37
Fredy Morales <saiberz@hotmail.com>
2014-10-21 18:01:16 -0500

    Sound and stuffs added

--- script.js
+++ script.js
@@ -6,38 +6,47 @@ app.controller("init",function ($scope,$resource){
                             { callback: "JSON_CALLBACK" },
                             { get: { method: "JSONP" }});
     var WordsList = [];
-
+    $scope.list  = [];
+    var audioElement = document.getElementById('audio');
     var newWord = function (){
         WordAPI.get().$promise.then(
             function(data){
                 WordsList.push(data.Word);
+                $scope.list = WordsList;
             }
-        );
+        );        
     };
 
     newWord();
     newWord();
 
     var actual = $scope.focus;
-
+    
     $scope.Start = function() {
         $scope.focus = WordsList[0];
-        actual = $scope.focus;
+        actual = $scope.focus;        
+        window.setInterval(function(){
+            newWord();
+            console.log("new word");
+        }, 2000);
+    
+        
     };
 
 
-
-    $scope.keypress = function(keyEvent) {
-        if(keyEvent.key == $scope.focus[0]){
-            console.log(WordsList);
+     $scope.keypress = function(keyEvent) {         
+        if(keyEvent.key == $scope.focus[0]){            
             actual = actual.substr(1,actual.length);
-            if(actual.length == 2){
-                WordsList.splice(0,1);
-                newWord();
+            if(actual.length == 2){                
+                WordsList.splice(0,1);                
                 actual = WordsList[0];
                 $scope.focus = actual;
             }
         }
+         else {
+             audioElement.currentTime = 0;
+             audioElement.play();
+         }
         $scope.focus = actual;
     };
 
